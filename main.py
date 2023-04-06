f39f8119215794b0b7d18fb62fa2682bce6ed236
Sombody <hamdimoubrak@mail.ru>
2023-04-06 23:30:26 +0200

    added task scheduler

--- main.py
+++ main.py
@@ -4,12 +4,31 @@ from Players import Players #useful to manipulate players instance
 from Player import Player
 import time
 from Exploit import roblox
-DataModel = Instance(GetDataModel())
-workspace = DataModel.GetChildren()[0]
-Players = Players(DataModel.FindFirstChild("Players"))
-shared_instances["Workspace"] = workspace
 
-SetupOptimizations()
-RenderJob = GetRenderJob()
-roblox.Program.write_bool(RenderJob+0x154, True) #Disable renderjob
-FreeOptimizations() # free the memory
+class Job():
+    def __init__(self, addr = 0) -> None:
+        self.addr = addr
+    def GetName(self) -> str:
+        return roblox.ReadNormalString(roblox.DRP(self.addr) + 0x10)
+    
+class TaskScheduler():
+    def __init__(self) -> None:
+        self.addr = roblox.DRP(roblox.getAddressFromName("RobloxPlayerBeta.exe+3DE457C"))
+    def getJobs(self) -> list[Job]:
+        schduler_start = roblox.DRP(self.addr + 0x134)
+        schduler_end = roblox.DRP(self.addr + 0x138)
+        returned = []
+        while schduler_start != schduler_end:
+            returned.append(Job(roblox.DRP(schduler_start)))
+            schduler_start += 8
+
+
+
+
+#SetupOptimizations()
+#RenderJob = GetRenderJob()
+#roblox.Program.write_bool(RenderJob+0x154, True) #Disable renderjob
+#localPlayer = Players.GetLocalPlayerChar(workspace)
+#humanoid = localPlayer.FindFirstChild("Humanoid")
+#humanoid.SetProperty("Sit", 1)
+#FreeOptimizations() # free the memory
